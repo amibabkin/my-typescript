0042fe3466cb9d406bc9687e570543e1c5e92c0b
Andrey <amibabkin@gmail.com>
2024-05-29 18:24:05 +0700

    feat: create TupleToObject type

--- src/developing/mapped-types.ts
+++ src/developing/mapped-types.ts
@@ -18,15 +18,18 @@ type OmitItem = MyOmit<Item, 'a'>
 type MyReadonly<T> = {
   readonly [P in keyof T]: T[P] 
 }
-
 interface Todo {
   title: string
   description: string
 }
-
 const todo: MyReadonly<Todo> = {
   title: 'hello',
   description: 'foo'
 }
-
 // todo.description = 'foo1'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+type TupleToObject<T extends readonly string[]> = {
+  [P in T[number]]: P
+}
+type result = TupleToObject<typeof tuple> // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
