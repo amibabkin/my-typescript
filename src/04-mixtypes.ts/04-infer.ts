013ef9bb978dc917f7620966ab9ec0301a434da2
Andrey <amibabkin@gmail.com>
2024-05-30 15:35:29 +0700

    feat: add infer

--- src/04-mixtypes.ts/04-infer.ts
+++ src/04-mixtypes.ts/04-infer.ts
@@ -17,3 +17,13 @@ type FirstArg<T> = T extends (first: infer First, ...arg: any[]) => any
 const myPair: FirstArg<typeof fromPair> = ["myKey", "myValue"];
 
 fromPair(myPair);
+
+// ------------------------------------
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type First1<T extends unknown[]> = T extends [] ? never : T[0]
+type First2<T extends unknown[]> = T extends [infer Item, ...unknown[]] ? Item : never
+
+type head1 = First1<arr1> // expected to be 'a'
+type head2 = First2<arr2> // expected to be 3
